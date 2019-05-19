f66938acaf19ec7cb19722611ccff5e9239f63a6
david.zuzunaga@fractal.local <david.zuzunaga@fractal.local>
2019-05-19 02:32:53 -0500

    Inicial

---
+++ Spring-micro-ui/js/index.js
@@ -0,0 +1,42 @@
+function showPassword() {
+    
+        var key_attr = $('#key').attr('type');
+    
+        if (key_attr != 'text') {
+    
+            $('.checkbox').addClass('show');
+            $('#key').attr('type', 'text');
+    
+        } else {
+    
+            $('.checkbox').removeClass('show');
+            $('#key').attr('type', 'password');
+    
+        }
+    
+    }
+    
+    $(document).ready(function () {
+        $("#btn-login").click(function () {
+            let correo = document.getElementById("email").value;        
+            $.ajax({
+                type: "GET",
+                url: "http://localhost:8080/persona/leerCorreo/" + correo,
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (data) {                
+                
+                    if(data > 0){
+                        sessionStorage.setItem("id", data);
+                        window.location.href = "./principal.html";
+                    }else{
+                        toastr.warning("Credenciales incorrectas");
+                    }
+                },
+    
+                error: function (XMLHttpRequest, textStatus, errorThrown) {
+                    toastr.error("Request: " + XMLHttpRequest.toString() + "\n\nStatus: " + textStatus + "\n\nError: " + errorThrown);
+                }
+            });
+        });
+    });
---
+++ Spring-micro-ui/js/principal.js
@@ -0,0 +1,86 @@
+$(document).ready(function () {
+    var id = sessionStorage.getItem("id");        
+
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/persona/leer/" + id,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (models) {
+            var nombre_completo = models.nombre_completo;
+            var pais = models.pais;
+            var bio = models.bio;
+            var foto = models.fotoURL;
+
+            var headerName = $('[id$=headerName');
+            var lblPais = $('[id$=lblPais');
+            var lblBio = $('[id$=lblBio');
+            $("#lblFoto").attr("src", foto);
+
+            headerName.html(nombre_completo);
+            lblPais.html(pais);
+            lblBio.html(bio);
+        },
+
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            toastr.error("Request: " + XMLHttpRequest.toString() + "\n\nStatus: " + textStatus + "\n\nError: " + errorThrown);
+        }
+    });
+
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/habilidad/listar/" + id,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (models) {
+            for (var i = 0; i < models.length; i++) {                      
+                
+                var exp = models[i].experiencia;
+                var span;
+
+                if(exp === "A"){
+                    span = $('<span />').addClass('label label-success').html(models[i].nombre); 
+                }else if(exp === "B"){
+                    span = $('<span />').addClass('label label-info').html(models[i].nombre); 
+                }else if(exp === "R"){
+                    span = $('<span />').addClass('label label-warning').html(models[i].nombre); 
+                }
+                
+                $("#lstHabilidades").append(span);
+            }            
+        },
+
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            toastr.error("Request: " + XMLHttpRequest.toString() + "\n\nStatus: " + textStatus + "\n\nError: " + errorThrown);
+        }
+    });
+
+    $("#btnCerrar").click(function () {
+        sessionStorage.clear();
+        window.location.href = "./index.html"
+    });
+
+    $(document).on('click', "#btnEnviar", function (e) {        
+        $.ajax({
+            type: "GET",
+            url: "https://mito-zuul-sp.herokuapp.com/node-client/getURL",
+            //retornara https://yt3.ggpht.com/-svmpAtQZ_Tc/AAAAAAAAAAI/AAAAAAAAAAA/41N37I0zCko/s900-c-k-no-mo-rj-c0xffffff/photo.jpg
+            async: false,
+            dataType: "text",
+            success: function (data) {
+                if (data == 'no_data') {
+                    toastr.warning("No existe una constancia generada");
+                } else {
+                    window.open(
+                        data
+                    );
+                }
+            },
+
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                console.log("Request: " + XMLHttpRequest.toString() + "\n\nStatus: " + textStatus + "\n\nError: " + errorThrown);
+            }
+        });
+    });
+
+});
